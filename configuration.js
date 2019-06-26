09c1936014c94b20dcadea98f39f9aa08182d6b7
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2019-06-26 09:34:59 +0200

    add configuration

---
+++ configuration.js
@@ -0,0 +1,13 @@
+module.exports =  {
+    poolFactoryAddress: '0x4cc4f956d39ff5f360343e453ab7c3f4cfadc593',
+    poolLibraryAddress: '0x1A48393ba853b9e1DE7Fc1c041FDaAf04af52454',
+    dxAddress: '0x7b7DC59ADBE59CA4D0eB32042fD5259Cf5329DE1',
+    dxProxyAddress: '0xaaeb2035ff394fdb2c879190f95e7676f1a9444b',
+    wethAddress: '0xc778417e063141139fce010982780140aa0cd5ab',
+    etherscan: 'https://rinkeby.etherscan.io/',
+    poolxUrl: 'https://poolx.org/',
+    nodeConnection:
+      "https://rinkeby.infura.io/v3/2f72e1e9d96b4a018cd94093bf7006d7",
+    numberOfConfirmations: 2,
+    botNumber: "622307864:AAHIhnEhS-WhA-ElEvbcCSOCU1Hj8Xb_zUM"
+}
